916d22d29fcfae6aff7a06c9ddb8a6421a65417b
mpolaczyk <marcin.polaczyk@gmail.com>
2023-04-22 00:18:32 +0200

    Bugfix + new binary.

--- RayTracer/processing/async_renderer_base.cpp
+++ RayTracer/processing/async_renderer_base.cpp
@@ -102,7 +102,7 @@ void async_renderer_base::async_job()
 
     if (save_output)
     {
-      char image_file_name[100];
+      char image_file_name[300];  // Run-Time Check Failure #2 - Stack around the variable 'image_file_name' was corrupted.
       std::sprintf(image_file_name, paths::get_render_output_file_path().c_str());
 
       benchmark::instance benchmark_save;
